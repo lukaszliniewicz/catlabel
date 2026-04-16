5435e26402951f01462e7552495142dec756d9b7
l.liniewicz <l.liniewicz@gmail.com>
2026-04-16 03:37:57 +0200

    cleanup

--- catlabel/devices/__init__.py
+++
@@ -1,4 +0,0 @@
-from .models import PrinterModel, PrinterModelRegistry
-from .resolve import DeviceResolver, ResolvedBluetoothDevice
-
-__all__ = ["DeviceResolver", "ResolvedBluetoothDevice", "PrinterModel", "PrinterModelRegistry"]
